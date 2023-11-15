5965e9fcecedb02e0b2b0a9038ce22400d7b4f96
Dan Fornika <dfornika@gmail.com>
2023-11-15 10:55:59 -0800

    Change sort order of uploaded runs table (and add some docs, implement figwheel hot-reloading

--- src/irida_upload_monitor/core.cljs
+++ src/irida_upload_monitor/core.cljs
@@ -1,4 +1,4 @@
-(ns irida-upload-monitor.core
+(ns ^:figwheel-hooks irida-upload-monitor.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.set]
             [reagent.core :as r] 
@@ -14,38 +14,37 @@
 
 (def app-version "v0.1.0")
 
-(defn in? 
-  "true if coll contains elem"
-  [coll elem]  
-  (some #(= elem %) coll))
-
-
-(defn load-uploaded-sequencing-runs []
-  ""
+(defn load-uploaded-sequencing-runs 
+  "Pull the list of uploaded sequencing runs from the server and loads them into the db"
+  []
   (go
     (let [response (<! (http/get "data/uploaded_sequencing_runs.json"))]
       (if (vector? (:body response))
         (swap! db assoc :uploaded_sequencing_runs (:body response))))))
 
 
-(defn load-uploaded-sequencing-run [run-id]
-  ""
+(defn load-uploaded-sequencing-run
+  "Pull a specific uploaded sequencing run from the server, based on its run-id
+   and loads it into the db"
+  [run-id]
   (go
     (let [response (<! (http/get (str "data/uploaded_sequencing_runs/" run-id ".json")))]
       (if (vector? (:body response))
         (swap! db assoc-in [:libraries-by-sequencing-run-id run-id] (:body response))))))
 
 
-(defn get-selected-rows [e]
+(defn get-selected-rows
   "Takes an event e from ag-grid and returns the selected rows as a vector of maps"
+  [e]
   (map #(js->clj (.-data %) :keywordize-keys true)
        (-> e
            .-api
            .getSelectedNodes)))
 
 
-(defn run-selected [e]
-  ""
+(defn run-selected
+  "Take an event e from ag-grid and load the selected sequencing run into the db"
+  [e]
   (let [previously-selected-run-id (:selected-sequencing-run-id @db)
         currently-selected-run-id (:sequencing_run_id (first (get-selected-rows e)))]
     (do
@@ -53,7 +52,9 @@
       (swap! db assoc :selected-sequencing-run-id currently-selected-run-id))))
 
 
-(defn header []
+(defn header
+  "Header component"
+  []
   [:header {:style {:display "grid"
                     :grid-template-columns "repeat(2, 1fr)"
                     :align-items "center"
@@ -66,7 +67,9 @@
     [:img {:src (str "images/bccdc_logo.svg") :height "48px"}]]])
 
 
-(defn uploaded-sequencing-runs-table []
+(defn uploaded-sequencing-runs-table
+  "Component for table of uploaded sequencing runs"
+  []
   (let [runs (:uploaded_sequencing_runs @db)
         row-data runs]
     [:div {:class "ag-theme-balham"
@@ -78,12 +81,14 @@
        :enableCellTextSelection true
        :onFirstDataRendered #(-> % .-api .sizeColumnsToFit)
        :onSelectionChanged run-selected}
-      [:> ag-grid/AgGridColumn {:field "sequencing_run_id" :headerName "Run ID" :minWidth 180 :resizable true :filter "agTextColumnFilter" :floatingFilter true :checkboxSelection true :sortable true :sort "inc"}]
+      [:> ag-grid/AgGridColumn {:field "sequencing_run_id" :headerName "Run ID" :minWidth 180 :resizable true :filter "agTextColumnFilter" :floatingFilter true :checkboxSelection true :sortable true :sort "desc"}]
       [:> ag-grid/AgGridColumn {:field "timestamp_upload_started" :headerName "Upload Started" :minWidth 180 :resizable true :filter "agTextColumnFilter" :floatingFilter true :sortable true}]
       [:> ag-grid/AgGridColumn {:field "timestamp_upload_completed" :headerName "Upload Completed" :minWidth 180 :resizable true :filter "agTextColumnFilter" :floatingFilter true :sortable true}]]]))
 
 
-(defn uploaded-libraries-table []
+(defn uploaded-libraries-table
+  "Component for table of uploaded libraries"
+  []
   (let [currently-selected-run (:selected-sequencing-run-id @db)
         libraries (get-in @db [:libraries-by-sequencing-run-id currently-selected-run] [])
         row-data libraries]
@@ -103,7 +108,9 @@
       [:> ag-grid/AgGridColumn {:field "remote_project_name" :headerName "Remote Project Name" :minWidth 180 :resizable true :filter "agTextColumnFilter" :floatingFilter true :sortable true}]]]))
 
 
-(defn root []
+(defn root
+  "Root component"
+  []
   [:div {:style {:display "grid"
                  :grid-template-columns "1fr"
                  :grid-gap "4px 4px"
@@ -124,8 +131,24 @@
                   :gap "4px"}}
     [uploaded-libraries-table]]]])
 
-(defn main []
-  (rdom/render [root] (js/document.getElementById "app"))
+
+(defn render
+  "Render the root component, and all of its children"
+  []
+  (rdom/render [root] (js/document.getElementById "app")))
+
+
+(defn ^:after-load re-render
+  "Re-render the root component, and all of its children.
+   Called by figwheel when a file is changed."
+  []
+  (render))
+
+
+(defn main
+  []
+  "Main function, called when the page is loaded."
+  (render)
   (load-uploaded-sequencing-runs))
 
 (set! (.-onload js/window) main)
